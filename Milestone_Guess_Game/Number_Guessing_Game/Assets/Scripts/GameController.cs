e4ed089ceaedac5d1654b5ee57119483d9bf909f
tashenea <tmb15@humboldt.edu>
2020-03-23 17:42:04 -0700

    Paused Project 3/23/2020
    Signed-off-by: tashenea <tmb15@humboldt.edu>

---
+++ Milestone_Guess_Game/Number_Guessing_Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public void GetInput(string Guess)
+    {
+        Debug.Log("You Entered" + Guess);
+    }
+
+
+
+
+
+}
